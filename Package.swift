3c82bbe38224fc2ace8dd36f0916ecc57aae5239
Mefilt <mefilt@gmail.com>
2025-10-09 15:09:45 +0300

    Update turf package version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/flitsmeister/mapbox-directions-swift", exact: "0.23.3"),
-        .package(url: "https://github.com/flitsmeister/turf-swift", exact: "0.2.2"),
+        .package(url: "https://github.com/mefilt/turf-swift.git", exact: "0.2.3"),
         .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution.git", from: "6.0.0"),
         .package(url: "https://github.com/mefilt/Solar.git", exact: "3.0.4"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.53.6")
